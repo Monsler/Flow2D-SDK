7404c4922f91cb1cf363d04215cf03bc4ece6f53
Monsler <105060825+Monsler@users.noreply.github.com>
2024-03-14 22:07:46 +0300

    2.5
    new method - get engine; now you can execute lua code to flow2d's scripting engine!

--- src/org/flow/Flow2D.java
+++ src/org/flow/Flow2D.java
@@ -1,12 +1,22 @@
 package org.flow;
 
+import org.luaj.vm2.script.LuaScriptEngine;
+
 import javax.swing.*;
 
 public class Flow2D {
 
     private static JPanel pane;
+    private static LuaScriptEngine engine;
     public static JPanel getPanel(){
         return pane;
+    }
+    public static void setEngine(LuaScriptEngine repl){
+        engine = repl;
+    }
+
+    public static LuaScriptEngine getEngine(){
+        return engine;
     }
 
     public static void setPanel(JPanel one){
